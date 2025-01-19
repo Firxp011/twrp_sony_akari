ca04e3f8a94a3f297a97a8d25d14936d313a34f4
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-01-19 08:53:41 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,6 +36,25 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
+	 # Version & Variant
+	 export FOX_VERSION="R12.1"
+	 export FOX_VARIANT="A14"
+	 export FOX_BUILD_TYPE="Stable"
+
+	# A/B partitioning
+	 export FOX_AB_DEVICE=1
+
+	 # OrangeFox Addons
+	 export FOX_ENABLE_APP_MANAGER=1
+	
+	 # Binaries & Tools
+	 export FOX_USE_BASH_SHELL=1
+	 export FOX_ASH_IS_BASH=1
+	 export FOX_USE_NANO_EDITOR=1
+	 export FOX_USE_TAR_BINARY=1
+	 export FOX_USE_SED_BINARY=1
+	 export FOX_USE_XZ_UTILS=1
+	
 
 
 	lunch twrp_$FDEVICE-eng
