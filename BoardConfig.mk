25d968ce42a5e1998fe5cda605d0550d8d86028b
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-01-19 09:58:53 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,9 @@ BOARD_KERNEL_CMDLINE := \
 
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
+PRODUCT_COPY_FILES += \
+    $(TARGET_OUT_INTERMEDIATES)/kernel:$(TARGET_OUT)/target/product/$(TARGET_DEVICE)/kernel
+
 # Prebuilt
 #TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 
